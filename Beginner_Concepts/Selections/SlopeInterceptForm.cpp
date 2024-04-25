686717a5e7ea66b0249b74434755f8dc4b302152
N00B-93 <omosheye007@gmail.com>
2024-04-25 20:21:14 +0100

    Add the SlopeInterceptForm.cpp source file.

---
+++ Beginner_Concepts/Selections/SlopeInterceptForm.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <iomanip>
+
+/**
+    This is a program that prompts the user to enter the coordinates of two points (x1, y1) and (x2, y2), and displays the line equation in the slope-intercept form, i.e., y = mx + b.
+                m = (y2 - y1)/(x2 - x1) and b = y1 - mx1
+*/
+
+int main(void)
+{
+    // Declares variable to hold the coordinates of the two points.
+    float x1, y1, x2, y2;
+
+    // Prompts the user to enter the coordinates of the first point.
+    std::cout << "\nEnter the x and y coordinate of the first point: ";
+    std::cin >> x1 >> y1;
+
+    // Prompts the user to enter the coordinates of the second point.
+    std::cout << "\nEnter the x and y coordinate of the second point: ";
+    std::cin >> x2 >> y2;
+
+    // Calculates the slope of the line.
+    float m = y2 - y1 / (x2 - x1);
+
+    // Calculates the y-intercept of the line.
+    float b = y1 - m * x1;
+
+    // Displays the equation of the line.
+    std::setprecision(4);
+    if (m == 1 and b == 0)
+        std::cout << "\nThe line connecting (" << x1 << "," << " " << y1 << ") and (" << x2 << "," << " " << y2 << ")  is: " << " y = x\n";
+    else if (m == 1)
+        std::cout << "\nThe line connecting (" << x1 << "," << " " << y1 << ") and (" << x2 << "," << " " << y2 << ") is: " << "y = x + " << b << "\n";
+    else if (b == 0)
+        std::cout << "\nThe line connecting (" << x1 << "," << " " << y1 << ") and (" << x2 << "," << " " << y2 << ") is: " << "y = " << m << "x\n";
+    else
+        std::cout << "\nThe line connecting (" << x1 << "," << " " << y1 << ") and (" << x2 << "," << " " << y2 << ") is: " << "y = " << m << "x" << " + " << b << "\n";
+    
+    return (0);
+}
