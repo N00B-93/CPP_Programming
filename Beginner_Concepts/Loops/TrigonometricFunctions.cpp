4106cc118537a19b29c641029c55a7371d52a586
N00B-93 <omosheye007@gmail.com>
2024-04-29 16:10:22 +0100

    Add the TrigonometricFunctions.cpp source file.

---
+++ Beginner_Concepts/Loops/TrigonometricFunctions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#define PI 3.14159f
+
+/**
+    This is a program that displays the values of tan and cot
+    in the range 1 - 60 in steps of 10 degrees.
+*/
+
+int main(void)
+{
+    std::cout << "\nDegree\t\tsin/cos\t\tcos/sin\n";
+    for (int i = 0; i <= 60; i += 10)
+    {
+        float radians = i * PI / 180.0;
+        std::cout << std::fixed << std::setprecision(4) << i << "\t\t" << sin(radians) / cos(radians) << "\t\t" << cos(radians) / sin(radians) << "\n";
+    }
+}
