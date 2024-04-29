bf78d8f3c3f23c30c261e97f71ef85db6de22d35
N00B-93 <omosheye007@gmail.com>
2024-04-29 16:10:56 +0100

    Add the ExponentialFunction.cpp source file.

---
+++ Beginner_Concepts/Loops/ExponentialFunction.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+
+/**
+    This is a program that prints a table of the exponent of 
+    integers from 1 - 10 using the exp function.
+*/
+
+int main(void)
+{
+    std::cout << "\nNumber\t\tExponent\n";
+    for (int i = 0; i <= 10; ++i)
+    {
+        std::cout << std::setprecision(6) << i << "\t\t" << exp(i) << "\n";
+    }
+}
