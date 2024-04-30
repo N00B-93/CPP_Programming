fa960709b11c8b011ea4283af0bb34bceba46b83
N00B-93 <omosheye007@gmail.com>
2024-04-30 11:09:01 +0100

    Add the FutureApartmentRent.cpp source file.

---
+++ Beginner_Concepts/Loops/FutureApartmentRent.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+/**
+    This is a program that computes the rent in five years and the total rent for one year starting five years 
+    from now, suppose that the current rent is $1,000 and it increases by 3% annually.
+*/
+
+int main(void)
+{
+    // Initailize a constant variable to hold the percent increment of the rent.
+    const float INCREMENT_PERCENT = 0.03F;
+
+    // Variable holding the current rent and total rent.
+    float currentRent = 1000.0f, totalRent = currentRent;
+
+    for (int i = 0; i < 4; i++)
+    {
+        currentRent += currentRent * INCREMENT_PERCENT;
+        totalRent += currentRent;
+    }
+
+    // Displays the total rent.
+    std::cout << "\nTotal Rent for 5 years: $" << totalRent;
+
+    // Displays the rent in the six year.
+    std::cout << "\nRent after 5 years: $" << currentRent + (currentRent * INCREMENT_PERCENT);
+
+    std::cout << "\n";
+}
