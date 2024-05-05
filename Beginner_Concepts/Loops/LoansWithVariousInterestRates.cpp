5722b67f46dd47a63b46e5af76b73711a6b1337c
N00B-93 <omosheye007@gmail.com>
2024-05-05 09:56:04 +0100

    Add documentation to the LoansWithVariousInterestRates.cpp source file.

--- Beginner_Concepts/Loops/LoansWithVariousInterestRates.cpp
+++ Beginner_Concepts/Loops/LoansWithVariousInterestRates.cpp
@@ -3,7 +3,9 @@
 #include <cmath>
 
 /**
-
+    This is a program that lets the user enter the loan amount and loan period in number of years 
+	and displays the monthly and total payments for each interest rate starting from 5% 
+	to 8%, with an increment of 1/8.
 */
 
 int main(void)
@@ -23,21 +25,27 @@ int main(void)
 	}
 	
 	// Variable that holds the interest rate lower limit.
-	float interestRateLimit = 5.00 / 100;
+	float interestRateLimit = 5.00;
 
-	std::cout << "\nInterest Rate\t\t\tMonthly Payment\t\t\tTotal Payment\n";
+	// Displays the interest rate, monthly payment and the total payment for each value of the interest rate in tabular form.
+	std::cout << "\nInterest Rate\t\tMonthly Payment\t\tTotal Payment\n";
 	while (interestRateLimit <= 8.00)
 	{
-		float monthlyInterestRate = interestRateLimit / 12;
+		// Calculates the monthly interest rate.
+		float monthlyInterestRate = interestRateLimit / 1200;
+
+		// Calculates the monthly payment.
 		float monthlyPayment = loanAmount * monthlyInterestRate / (1 - 1 / pow(1 + monthlyInterestRate, 12 * numberOfYears));
 
+		// Calculates the total payment.
 		float totalPayment = monthlyPayment * 12 * numberOfYears;
 
-		std::cout << std::fixed << std::setprecision(2) << interestRateLimit << "\t\t\t" << monthlyPayment << "\t\t\t" << totalPayment << "\n";
+		// Displays the interest rate, monthly payment and the total amount for each interest rate.
+		std::cout << std::fixed << std::setprecision(3) << interestRateLimit << "\t\t\t" << monthlyPayment << "\t\t\t" << totalPayment << "\n";
 
-		interestRateLimit += 0.125 / 100;
+		// Increments the interest rate limit by 0.125.
+		interestRateLimit += 0.125;
 	}
 
 	return (0);
 }
-
