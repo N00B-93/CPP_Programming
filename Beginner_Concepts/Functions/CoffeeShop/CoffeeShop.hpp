4c9fdecb201e89bda080d347d280655873402441
N00B-93 <omosheye007@gmail.com>
2024-05-08 18:57:06 +0100

    Add theCoffeeShop.hpp header file.

---
+++ Beginner_Concepts/Functions/CoffeeShop/CoffeeShop.hpp
@@ -0,0 +1,12 @@
+#include <string>
+#ifndef COFFEESHOP_HPP
+#define COFFEESHOP_HPP
+
+
+void displayMainMenu();
+void displayCoffeeMenu();
+void displayNumberOfCupOfEachSizeSold(int, int, int);
+void displayTotalAmountOfCoffeeSold(int, int, int, const int, const int, const int);
+void displayTotalAmountOfMoneyMade(int, int, int, const double, const double, const double);
+
+#endif /* COFFEESHOP_HPP */
