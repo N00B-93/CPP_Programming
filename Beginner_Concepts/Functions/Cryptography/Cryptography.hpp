74d0adcb202f852d82d97b8b07a8d3bff5a7fa65
N00B-93 <omosheye007@gmail.com>
2024-05-12 12:01:58 +0100

    Add the Cryptography.hpp header file.

---
+++ Beginner_Concepts/Functions/Cryptography/Cryptography.hpp
@@ -0,0 +1,9 @@
+#include <string>
+#ifndef CRYPTOGRAPY_HPP
+#define CRYPTOGRAPY_HPP
+
+std::string swap(std::string, int, int, int, int);
+std::string encrypt4Digit(std::string);
+std::string decrypt4Digit(std::string);
+
+#endif /* CRYPTOGRAPY_HPP */
