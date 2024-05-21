26d5f21e17688f84c248af2878445176b7cf86f9
N00B-93 <omosheye007@gmail.com>
2024-05-21 19:34:47 +0100

    Add the RainfallStatistics.cpp source file.

--- Beginner_Concepts/Single_Dimensional_Arrays_And_C-Strings/RainfallStatistics.cpp
+++ Beginner_Concepts/Single_Dimensional_Arrays_And_C-Strings/RainfallStatistics.cpp
@@ -8,6 +8,9 @@
 	the average monthly rainfall, and the months with the highest and lowest amounts.
 */
 
+int getIndexOfMaximumValue(double array[], int size);
+int getIndexOfMinimumValue(double array[], int size);
+
 int main(void)
 {
 	// Constant holding the number of month.
@@ -50,7 +53,15 @@ int main(void)
 
 	// Displays the average monthly rainfall.
 	std::cout << std::fixed << std::setprecision(2) << "\nThe average monthly rainfall is: " << totalRainfall / NUMBER_OF_MONTHS << "\n";
-	
+
+  // Determines the index of the month with the largest and lowest rainfall.
+  int indexOfMonthWithLargestRainfall = getIndexOfMaximumValue(monthlyRainfall, NUMBER_OF_MONTHS);
+  int indexOfMonthWithSmallestRainfall = getIndexOfMinimumValue(monthlyRainfall, NUMBER_OF_MONTHS);
+
+  // Displays the months with the maximum and minimum rainfall.
+  std::cout << "\nMonth with maximum rainfall: " << months[indexOfMonthWithLargestRainfall] << "\n";
+  std::cout << "\nMonth with minimum rainfall: " << months[indexOfMonthWithSmallestRainfall] << "\n";
+
 	return (0);
 }
 
@@ -61,7 +72,7 @@ int main(void)
  * @param size The number of elements in the array.
  * @return The index of the maximum element in an array of double values.
  */
-int getIndexOfMaximumValue(const double array, const int size)
+int getIndexOfMaximumValue(double array[], int size)
 {
 	int index = 0;
 	double maximumElement = array[0];
@@ -71,6 +82,30 @@ int getIndexOfMaximumValue(const double array, const int size)
 		if (array[i] > maximumElement)
 		{
 			maximumElement = array[i];
+			index = i;
+		}
+	}
+
+	return index;
+}
+
+/**
+ * This determines the index of the element with the lowest value in an array of double values.
+ *
+ * @param array The array of double values.
+ * @param size The number of elements in the array.
+ * @return The index of the minimum element in an array of double values.
+ */
+int getIndexOfMinimumValue(double array[], int size)
+{
+	int index = 0;
+	double minimumElement = array[0];
+
+	for (int i = 0; i < size; ++i)
+	{
+		if (array[i] < minimumElement)
+		{
+			minimumElement = array[i];
 			index = i;
 		}
 	}
