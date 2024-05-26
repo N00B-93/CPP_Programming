584d4a9c68266a5ee50d8b120f81f29069d6137a
N00B-93 <omosheye007@gmail.com>
2024-05-26 19:53:13 +0100

    Add the CountOccurrenceOfNumbers.cpp source file.

---
+++ Beginner_Concepts/Single_Dimensional_Arrays_And_C-Strings/CountOccurrenceOfNumbers.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+/**
+    This is  a program that reads at most 100 integers between 1 and 100 and counts the occurrence of each number. Assuming 
+    that the input ends with 0.
+*/
+
+int main (int argc, char *argv[])
+{
+  // Initialize and declares variables used in the program.
+  const int MAX_VALUE = 100, MAX_INPUT = 100;
+  int count[MAX_VALUE + 1] = {0};
+  int totalNumberOfInput{}, number;
+
+  // Reads in up to 100 integers from 1 - 100.
+  std::cout << "\nEnter up to " << MAX_INPUT << " integers between 1 - " << MAX_VALUE << ": ";
+  while (totalNumberOfInput < MAX_INPUT)
+  {
+    std::cin >> number;
+
+    if (number == 0)  // Vreaks out of the loop if the user enters 0.
+      break;
+    else if (number < 0 || number > 100)  // Goes to the next iteration if the number is < 1 or > 100.
+    {
+      std::cout << "\nError: Use number in the range 1 - 100 only, Try again.\n";
+      continue;
+    }
+
+    totalNumberOfInput++;
+    count[number]++;  // Increments the count for the number.
+  }
+
+  // Displays an error message and terminates the program if the user's uses a non integer input.
+  if (std::cin.fail())
+  {
+    std::cout << "\nError: Use integers from 1 - 100 only, Try again.\n";
+    exit(EXIT_FAILURE);
+  }
+
+  // Displays the number of occurrence of each integer in the array.
+  for (int i = 1; i <= MAX_VALUE; ++i)
+  {
+    if (count[i] == 1)
+      std::cout << "\n" << i << " occurs " << count[i] << " time.\n";
+    else if (count[i] > 1)
+      std::cout << "\n" << i << " occurs " << count[i] << " times.\n";
+  }
+
+  return (0);
+}
