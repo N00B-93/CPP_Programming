87675da6d17db10b220ff40f5be2656d2cc259b2
N00B-93 <omosheye007@gmail.com>
2024-05-28 21:25:34 +0100

    Add the Fan.cpp implementation file.

---
+++ Beginner_Concepts/Objects_And_Classes/Fan/Fan.cpp
@@ -0,0 +1,12 @@
+#include "Fan.hpp"
+
+/**
+ * Un-parameterized constructor that instantiates a new Fan with default values.
+ */
+Fan::Fan()
+{
+	on = false;
+	fanSpeed = SLOW;
+	radius = 5.0;
+}
+	
