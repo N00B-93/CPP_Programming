ae473b4f5aced106496214002a1210eff985f038
N00B-93 <omosheye007@gmail.com>
2024-05-28 20:19:08 +0100

    Add the Main.cpp source file to test the Rectangle class.

---
+++ Beginner_Concepts/Objects_And_Classes/Rectangle/Main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "Rectangle.hpp"
+
+void getDetails(Rectangle rectangle);  // Prototype of the getDetails function.
+
+int main(int argc, char* argv[])
+{
+	// Creates two Rectangle Objects.
+	Rectangle rectangle1(4, 40);
+	Rectangle rectangle2(3.5, 35.9);
+
+	// Displays the properties of each Rectangle.
+	std::cout << "\n\t\tRectangle Details";
+	getDetails(rectangle1);
+	getDetails(rectangle2);
+
+	return (0);
+}
+
+/**
+ * Displays the properties of the Rectangle Object.
+ *
+ * @param rectangle A Rectangle Object.
+ */
+void getDetails(Rectangle rectangle)
+{
+	std::cout << "\nWidth: " << rectangle.width;
+	std::cout << "\nHeight: " << rectangle.height;
+	std::cout << "\nPerimeter: " << rectangle.getPerimeter();
+	std::cout << "\nArea: " << rectangle.getArea();
+	std::cout << "\n";
+}
+
