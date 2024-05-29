6a7a6211f971db2d1fefb82fed77fe03bb3c80b6
N00B-93 <omosheye007@gmail.com>
2024-05-29 07:57:31 +0100

    Add the MyPoint.hpp definition file.

---
+++ Beginner_Concepts/Objects_And_Classes/MyPoint/MyPoint.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+/**
+ * The type MyPoint.
+*/
+class MyPoint
+{
+    public:
+        double xCoord, yCoord;
+
+        MyPoint();  // Un-parameterrized constructor's prototype.
+
+        MyPoint(double, double);  // Parameterized constructor's prototype.
+
+        static double getDistance(MyPoint, MyPoint);  // Prototype of the getDistance method.
+};
