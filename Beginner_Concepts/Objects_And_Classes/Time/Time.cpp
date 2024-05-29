7658e33c37fd7a2e9ec547d58cacedfac31289b1
N00B-93 <omosheye007@gmail.com>
2024-05-29 20:06:59 +0100

    Correct error in function documentation and define a helper function to convert total seconds to hours, minutes, and seconds

--- Beginner_Concepts/Objects_And_Classes/Time/Time.cpp
+++ Beginner_Concepts/Objects_And_Classes/Time/Time.cpp
@@ -1,6 +1,8 @@
 #include "Time.hpp"
 #include <ctime>
 
+void setMinsHoursAndSecs(long int, long int&, long int&, long int&);  // Prototype of the setMinsHoursAndSecs function.
+
 /**
  * Instantiates a Time Object with default values for the hours, minutes and seconds.
  */
@@ -9,37 +11,17 @@ Time::Time()
 	// Determines the number of seconds since the unix epoch.
 	long int totalSeconds = time(NULL);
 	
-	// Determines the current second, current minute and current hour.
-        long int currentSecond = totalSeconds % 60;
-        long int totalMinutes = totalSeconds / 60;
-        long int currentMinute = totalMinutes % 60;
-        long int totalHours = totalMinutes / 60;
-        long int currentHour = totalHours % 24;
-	
-	// Initialize the Time Object's fields with the current second, current minute and current hour.
-        hours = currentHour;
-        minutes = currentMinute;
-        seconds = currentSecond;
+        setMinsHoursAndSecs(totalSeconds, hours, minutes, seconds);
 }
 
 /**
  * Instantiates a new Time Object.
  *
- * @param The total seconds elapsed.
+ * @param totalSeconds The total seconds elapsed.
  */
 Time::Time(long int totalSeconds)
 {
-	// Determines the current second, current minute and current hour.
-        long int currentSecond = totalSeconds % 60;
-        long int totalMinutes = totalSeconds / 60;
-        long int currentMinute = totalMinutes % 60;
-        long int totalHours = totalMinutes / 60;
-        long int currentHour = totalHours % 24;
-	
-	// Initialize the Time Object's fields with the current second, current minute and current hour.
-        hours = currentHour;
-        minutes = currentMinute;
-        seconds = currentSecond;
+        setMinsHoursAndSecs(totalSeconds, hours, minutes, seconds);
 }
 
 /**
@@ -63,9 +45,22 @@ Time::Time(long int hoursElapsed, long int minutesElapsed, long int secondsElaps
  */
 void Time::setElapsedTime(long int elapsedTime)
 {
-    	// Determines the current second, current minute and current hour.
-        long int currentSecond = elapsedTime % 60;
-        long int totalMinutes = elapsedTime / 60;
+        setMinsHoursAndSecs(elapsedTime, hours, minutes, seconds);
+}
+
+/**
+ * Sets the hours, minutes and seconds of a Time Object to a new value based on a specified elapsed time.
+ * 
+ * @param totalSeconds The elapsed time in seconds.
+ * @param hour A variable to contain the hour derived from the elapsed time.
+ * @param minutes A variable to contain the minutes derived from the elapsed time.
+ * @param seconds A variable to contain the seconds derived from the elapsed time.
+*/
+void setMinsHoursAndSecs(long int totalSeconds, long int& hours, long int& minutes, long int& seconds)
+{
+        // Determines the current second, current minute and current hour.
+        long int currentSecond = totalSeconds % 60;
+        long int totalMinutes = totalSeconds / 60;
         long int currentMinute = totalMinutes % 60;
         long int totalHours = totalMinutes / 60;
         long int currentHour = totalHours % 24;
@@ -75,4 +70,3 @@ void Time::setElapsedTime(long int elapsedTime)
         minutes = currentMinute;
         seconds = currentSecond;
 }
-
