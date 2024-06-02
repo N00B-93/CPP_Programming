64fc73f081e3c8a385f913522de54a5cbb5f8f75
N00B-93 <omosheye007@gmail.com>
2024-06-02 03:19:30 +0100

    Add the StopWatch.hpp definition file.

---
+++ Beginner_Concepts/Objects_And_Classes/StopWatch/StopWatch.hpp
@@ -0,0 +1,19 @@
+/**
+ * The type StopWatch
+ */
+class StopWatch
+{
+	public:
+		StopWatch(); // No-arg constructor
+		long int getStartTime();  // Prototype of the getStartTime function.
+		long int getEndTime();  // Prototype of the getEndTime function.
+		void start();  // Prototype of the start function.
+		void stop();  // Prototype of the start function.
+		long int getElapsedTime();  // Prototype of the get elapsed time function.
+	
+	// Attributes of a StopWatch.
+	private:
+		long int startTime;
+		long int endTime;
+};
+
