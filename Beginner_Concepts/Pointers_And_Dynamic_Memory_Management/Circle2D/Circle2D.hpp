79a7df8d4a4f77f04510f6b7fcb8154ec0caf110
N00B-93 <omosheye007@gmail.com>
2024-06-08 11:25:43 +0100

    Add the Circle2D.hpp interface file.

---
+++ Beginner_Concepts/Pointers_And_Dynamic_Memory_Management/Circle2D/Circle2D.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+/**
+ *  The type Cirle2D.
+*/
+class Circle2D
+{
+    public:
+        Circle2D();  // No-arg constructor.
+        Circle2D(double, double, double);  // Parameterized constructor.
+        double getArea();  // Prototype of the getArea function.
+        double getPerimeter();  // Prototype of the getPerimeter function.
+        bool contains(double, double) const;  // Prototype of the overloaded contains function.
+        bool contains(const Circle2D&) const;  // Prototype of the overloaded contains function.
+        bool overlaps(const Circle2D&) const;  // Prototype of the overlap function.
+
+    // Attributes of a Circle2D Object.
+    private:
+        double yCoord, xCoord, radius;
+};
