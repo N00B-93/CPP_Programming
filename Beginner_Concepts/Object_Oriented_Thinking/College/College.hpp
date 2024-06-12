d17464c596dd1cb1f6954ca49593e738505e8197
N00B-93 <omosheye007@gmail.com>
2024-06-12 19:34:55 +0100

    Add more logic to the College.hpp interface file.

--- Beginner_Concepts/Object_Oriented_Thinking/College/College.hpp
+++ Beginner_Concepts/Object_Oriented_Thinking/College/College.hpp
@@ -7,15 +7,15 @@
 class Instructor
 {
 	public:
+		Instructor();
 		Instructor(std::string&, std::string&, int);  // Parameterized constructor prototype.
 		std::string getFirstName() const;  // Prototype of the getFirstName function.
 		std::string getLastName() const;  // Prototype of the getLastName function.
-		void setFirstName(std::string& firstName);  // Prototype of the setFirstName function.
-		void setLastName(std::string& LastName);  // Prototype of the setLastName.
 		int getOfficeNumber() const;  // Prototype of the getOfficeNumber function.
-		void setOfficeNumber(int& officeNumber);  // Prototype of the setOfficeNumber function.
-
-		void toString() const;  // Prototype of the toString function.
+		void setFirstName(std::string&);  // Prototype of the setFirstName function.
+		void setLastName(std::string&);  // Prototype of the setLastName.
+		void setOfficeNumber(int);  // Prototype of the setOfficeNumber function.
+		std::string toString() const;  // Prototype of the toString function.
 		
 	// Attributes of an Instructor.
 	private:
@@ -30,22 +30,35 @@ class Instructor
 class Classroom
 {
 	public:
-		Classroom(int&, int&);  // Parameterized constructor prototype.
+		Classroom();
+		Classroom(int, int);  // Parameterized constructor prototype.
 		int getBuildingNumber() const;  // Prototype of the getBuildingNumber function.
 		int getRoomNumber() const;  // Prototype of the getRoomNumber function.
-		void setBuildingNumber(int&)  // Prototype of the setBuilding number function.
-		void setRoomNumber(int&);  // Prototype of the setRoomNumber function.
-		void toString()  // Prototype of the toString function.
+		void setBuildingNumber(int);  // Prototype of the setBuilding number function.
+		void setRoomNumber(int);  // Prototype of the setRoomNumber function.
+		std::string toString() const;  // Prototype of the toString function.
 	
-	// Attributes of a Classroom.
-	private:
+		// Attributes of a Classroom.
+		private:
 		int buildingNumber;
 		int roomNumber;
-}
+};
 
 /**
  * The type CollegeCourse
  */
 class CollegeCourse
 {
+	public:
+		CollegeCourse(Instructor&, Classroom&, int);  // Parameterized constructor.
+		Instructor getInstructor() const;  // Prototype of the getInstructor function.
+		Classroom getClassroom() const;  // Prototype of the getClassroom function.
+		int getNumberOfCredits() const;  // Prototype of the getNumberOfCredits function.
+		std::string toString() const;  // Prototype of the toString function.
 
+	// Attributes of a CollegeCourse.
+	private:
+		Instructor instructor;
+		Classroom classroom;
+		int numberOfCredits;
+};
