75663b6d29d58d4074aca1deb61d8329bcd5992a
N00B-93 <omosheye007@gmail.com>
2024-06-20 16:47:44 +0100

    Add the Country.cpp implementation file.

---
+++ Beginner_Concepts/Objects_And_Classes/Country/Country.cpp
@@ -0,0 +1,66 @@
+#include "Country.hpp"
+#include <vector>
+
+/**
+ * Instantiates a new Country.
+ * 
+ * @param countryName The name of a Country.
+ * @param population The number of people living in a Country.
+ */
+Country::Country(std::string countryName, long int population, double landArea)
+{
+    this->countryName = countryName;
+    this->population = population;
+    this->landArea  = landArea;
+}
+
+/**
+ * This returns the number of people living in a Country.
+ * 
+ * @return A Country's population.
+ */
+long int Country::getPopulation() const
+{
+    return this->population;
+}
+
+/**
+ * This returns the name of a Country.
+ * 
+ * @return A Country's name.
+ */
+std::string Country::getCountryName() const
+{
+    return this->countryName;
+}
+
+/**
+ * This returns the land area of a Country.
+ * 
+ * @return A Country's land area.
+ */
+double Country::getLandArea() const
+{
+    return this->landArea;
+}
+
+/**
+ * Determines the Country with the highest Population density in a vector of Countries.
+ * 
+ * @param countries A vector containing Country Objects.
+ * @return A Country with the highest Population density.
+ */
+Country getCountryWithHighestPopulationDensity(std::vector<Country> countries)
+{
+    Country countryWithHighestDensity = countries[0];
+
+    for (Country country: countries)
+    {
+        double populationDensity = countryWithHighestDensity.getPopulation() / countryWithHighestDensity.getLandArea();
+
+        if ((country.getPopulation() / country.getLandArea()) > populationDensity)
+            countryWithHighestDensity = Country(country);
+    }
+
+    return countryWithHighestDensity;
+}
