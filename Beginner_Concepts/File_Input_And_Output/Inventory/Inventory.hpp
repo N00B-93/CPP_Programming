fed6bb7636585451b61bd01ce73f5c47cba89d97
N00B-93 <omosheye007@gmail.com>
2024-06-29 22:39:08 +0100

    Add the Inventory.hpp interface file for the Inventory structure.

---
+++ Beginner_Concepts/File_Input_And_Output/Inventory/Inventory.hpp
@@ -0,0 +1,56 @@
+#pragma once
+#include <string>
+
+// A struct representing an Inventory item.
+struct Inventory
+{
+	std::string itemName;
+	std::string dateAdded;
+	double retailPrice;
+	double wholesalePrice;
+	int quantityInStock;
+	int itemID;
+};
+
+/**
+ * Adds an Item to the file containing items.
+ *
+ * @param filePath The path to the file where the item to be added will be stored.
+ */
+void addItem(std::string& filePath);
+
+/**
+ * Search for an Inventory item from the item database.
+ *
+ * @param filePath The path to the item database.
+ */
+void searchItem(std::string& filePath);
+
+/**
+ * Update the details of an Item.
+ *
+ * @param filePath The path to the item database.
+ */
+void updateItem(std::string& filePath);
+
+/**
+ * Deletes an Item from the item database.
+ *
+ * @param filePath The path to the item database.
+ */
+void deleteItem(std::string filePath);
+
+/**
+ * Displays the information of all the items in the database.
+ *
+ * @param filePath The path to the item database.
+ */
+void displayAllItems(std::string filePath);
+
+/**
+ * Creates the itema database.
+ *
+ * @param filePath The path to the item database.
+ */
+void createDatabase(std::string filePath);
+
