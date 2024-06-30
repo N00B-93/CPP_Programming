fca06995e79a9b12a603f749c47a647992ba2c1b
N00B-93 <omosheye007@gmail.com>
2024-06-30 13:57:21 +0100

    Started writing logic for the driver program.

--- Beginner_Concepts/File_Input_And_Output/Inventory/Main.cpp
+++ Beginner_Concepts/File_Input_And_Output/Inventory/Main.cpp
@@ -2,12 +2,43 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <limits>
 
 int main(int argc, char const *argv[])
 {
-    // Creates a vector of Inventory items.
-    std::vector<Inventory> items;
+    	std::string itemDatabase("items.dat");
+
+	createDatabase(itemDatabase);
+	
+	while (true)
+	{
+		int choice;
+
+		displayMenu();
+		std::cout << "\nPlease select a choice: ";
+		std::cin >> choice;
+		
+		if (std::cin.fail() || choice < 1 || choice > 6)
+		{
+			std::cerr << "\nError: Invalid choice selected, Try again with a valid choice.\n";
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			continue;
+		}
+
+		switch (choice)
+		{
+			case 1:
+				addItem(itemDatabase);
+				break;
+			case 2:
+				searchItem(itemDatabase);
+				break;
+		}
+	}
+
+
 
     
-    return 0;
+    	return 0;
 }
