59990e49c36853307e25186a541ed793aa6c685c
N00B-93 <omosheye007@gmail.com>
2024-06-30 07:32:03 +0100

    Add the Main.cpp driver program.

---
+++ Beginner_Concepts/File_Input_And_Output/Inventory/Main.cpp
@@ -0,0 +1,13 @@
+#include "Inventory.hpp"
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+int main(int argc, char const *argv[])
+{
+    // Creates a vector of Inventory items.
+    std::vector<Inventory> items;
+
+    
+    return 0;
+}
