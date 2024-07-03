9e246073e340bb537906719e16e50c29c6670640
N00B-93 <omosheye007@gmail.com>
2024-07-03 23:42:21 +0100

    Add the CaseManipulator.hpp interface file.

---
+++ Beginner_Concepts/Enumerated_Types_Namespaces_And_Strings/CaseManipulator/CaseManipulator.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+
+/**
+ * Converts each character in a string to uppercase.
+ *
+ * @param str The string to be capitalized.
+ */
+void capitalize(std::string* str);
+
+/**
+ * Converts each character in a c-string to lowercase.
+ *
+ * @param str The string whose character is to be converted to lowercase.
+ */
+void toLowercase(std::string* str);
+
+/**
+ * Reverses the characters of a c-string.
+ *
+ * @param str The string whose characters are to be reversed.
+ */
+void reverseString(std::string* str);
+
