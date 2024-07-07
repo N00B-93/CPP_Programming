f4daa4209cebdfd6052d34568a892681ba771f8d
N00B-93 <omosheye007@gmail.com>
2024-07-07 10:01:34 +0100

    Add the Interface file
    Add the Interface file containing function definitions
    for the CaseManipulator project.

--- Beginner_Concepts/Enumerated_Types_Namespaces_And_Strings/CaseManipulator/CaseManipulator.hpp
+++ Beginner_Concepts/Enumerated_Types_Namespaces_And_Strings/CaseManipulator/CaseManipulator.hpp
@@ -2,23 +2,22 @@
 #include <string>
 
 /**
- * Converts each character in a string to uppercase.
+ * Converts each character in a c-string to uppercase.
  *
- * @param str The string to be capitalized.
+ * @param str The c-string to be capitalized.
  */
-void capitalize(std::string* str);
+void toUppercase(char* str);
 
 /**
  * Converts each character in a c-string to lowercase.
  *
- * @param str The string whose character is to be converted to lowercase.
+ * @param str The c-string whose character is to be converted to lowercase.
  */
-void toLowercase(std::string* str);
+void toLowercase(char* str);
 
 /**
- * Reverses the characters of a c-string.
+ * Reverses the case of characters of a c-string i.e. uppercase characters are converted to lowercase and vice versa.
  *
- * @param str The string whose characters are to be reversed.
+ * @param str The c-string whose characters' case is to be reversed.
  */
-void reverseString(std::string* str);
-
+void reverseCase(char* str);
