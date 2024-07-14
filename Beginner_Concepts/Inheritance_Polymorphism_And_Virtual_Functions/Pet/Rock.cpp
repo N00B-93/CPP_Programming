d29e2a4e21d9014c80e865d1351b94fd9df296c7
N00B-93 <omosheye007@gmail.com>
2024-07-14 22:21:36 +0100

    Add implementation file for the Rock class.

---
+++ Beginner_Concepts/Inheritance_Polymorphism_And_Virtual_Functions/Pet/Rock.cpp
@@ -0,0 +1,26 @@
+#include "Rock.hpp"
+
+/**
+ * No-arg constructor that sets the fields of a Rock to default values.
+ */
+Rock::Rock(){};
+
+/**
+ * Instantiates a new Rock.
+ *
+ * @param rockName The name of a Rock.
+ * @param rockAge The age of a Rock.
+ * @param rockWeight The weight of a Rock.
+ */
+Rock::Rock(std::string rockName, int rockAge, double rockWeight): Pet(rockName, rockAge, rockWeight){};
+
+/**
+ * This returns a string describing the lifespan of a Rock.
+ *
+ * @return A string describing the lifespan of a Rock.
+ */
+std::string Rock::getLifeSpan() const
+{
+	return "Thoudands of years.";
+}
+
