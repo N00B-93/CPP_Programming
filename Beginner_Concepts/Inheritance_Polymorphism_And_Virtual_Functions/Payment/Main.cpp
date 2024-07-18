83fd447cbb4b1d6bfe637821e57f6f55eddc5f04
N00B-93 <omosheye007@gmail.com>
2024-07-18 18:32:03 +0100

    Modify the Main.cpp driver program
    Add documentation to the Main.cpp driver program.

--- Beginner_Concepts/Inheritance_Polymorphism_And_Virtual_Functions/Payment/Main.cpp
+++ Beginner_Concepts/Inheritance_Polymorphism_And_Virtual_Functions/Payment/Main.cpp
@@ -4,10 +4,12 @@
 
 int main(int argc, char const *argv[])
 {
+    // Creates a Payment, CashPayment and CreditCardPayment Objects.
     Payment payment(2562.33);
     CashPayment cashPayment(513.45);
     CreditCardPayment creditCardPayment(78966.23, "Alabeni Omosheye", "30-06-2025", 79139880);
 
+    // Calls the paymentDetails functions of each Object.
     payment.paymentDetails();
     std::cout << "\n";
     cashPayment.paymentDetails();
