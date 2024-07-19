41f7a06d323f1c9d42c544175f98a0750c77c13b
N00B-93 <omosheye007@gmail.com>
2024-07-19 13:43:24 +0100

    Add interface file
    Add the HouseThatIsAnInvestment.hpp interface
    file for the HouseThatIsAnInvestment class.

---
+++ Beginner_Concepts/Inheritance_Polymorphism_And_Virtual_Functions/Investment/HouseThatIsAnInvestment.hpp
@@ -0,0 +1,12 @@
+#include "Investment.hpp"
+#include "House.hpp"
+
+class HouseThatIsAnInvestment: public Investment, public House
+{
+	// Utility functions of the HouseThatIsAnInvestment class.
+	public:
+		HouseThatIsAnInvestment();
+		HouseThatIsAnInvestment(double, double, std::string, double);
+		void display() const;
+};
+
