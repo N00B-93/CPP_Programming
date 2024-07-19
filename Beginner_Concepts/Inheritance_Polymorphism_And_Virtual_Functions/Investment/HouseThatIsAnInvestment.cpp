bf6ce6d2adbb1ddffae348cea6955accc8252f4d
N00B-93 <omosheye007@gmail.com>
2024-07-19 13:41:54 +0100

    Add implentation file
    Add HouseThatIsAnInvestment.cpp implementation
    file for the HouseThatIsAnInvestment class.

---
+++ Beginner_Concepts/Inheritance_Polymorphism_And_Virtual_Functions/Investment/HouseThatIsAnInvestment.cpp
@@ -0,0 +1,26 @@
+#include "HouseThatIsAnInvestment.hpp"
+
+/**
+ * No-arg constructor that initializes an HouseThatIsAnInvestment with default values.
+ */
+HouseThatIsAnInvestment::HouseThatIsAnInvestment(): Investment(), House(){};
+
+/**
+ * Instantiates a new HouseThatIsAnInvestment Object.
+ *
+ * @param initialValue The Initial value of an HouseThatIsAnInvestment.
+ * @param currentValue The current value of an HouseThatIsAnInvestment.
+ * @param address The address of an HouseThatIsAnInvestment.
+ * @param squareFeet The area covered by an HouseThatIsAnInvestment in square feet.
+ */
+HouseThatIsAnInvestment::HouseThatIsAnInvestment(double initialValue, double currentValue, std::string address, double squareFeet): Investment(initialValue, currentValue), House(address, squareFeet){};
+
+/**
+ * Displays the Properties of an JouseThatIsAnInvestment.
+ */
+void HouseThatIsAnInvestment::display() const
+{
+	this->Investment::display();
+	this->House::display();
+}
+
