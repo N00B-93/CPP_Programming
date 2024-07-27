20b80ab65ccf46038147b88082690fda01c81f01
N00B-93 <omosheye007@gmail.com>
2024-07-27 22:29:19 +0100

    Add Main.cpp driver program

---
+++ Beginner_Concepts/Exception_Handling/RealEstate/Main.cpp
@@ -0,0 +1,24 @@
+#include "RealEstate.hpp"
+#include <iostream>
+
+int main(int argc, char* const argv[])
+{
+	try
+	{
+		// Creates a RealEstate Object.
+		RealEstate realEstate;
+		
+		// Reads in field values of the RealEstate Object.
+		std::cin >> realEstate;
+		
+		// Displays the RealEstate Object.
+		std::cout << realEstate;
+	}
+	catch (std::runtime_error error)
+	{
+		std::cout << "\n" << error.what() << "\n";
+	}
+
+	return (0);
+}
+
